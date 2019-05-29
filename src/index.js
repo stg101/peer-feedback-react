02786c112de0f50915c9f7a163d6d42920671f43
Ricardo <ry.yrupailla@gmail.com>
2019-05-29 13:23:04 -0500

    It's in the bag

--- src/index.js
+++ src/index.js
@@ -1,12 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function MainContainer() {
+  const [pageIndex, setPageIndex] = React.useState("login");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  let returner = null;
+  switch (pageIndex) {
+    case "login":
+      returner = (
+        <LoginView pageIndex={pageIndex} setPageIndex={setPageIndex} />
+      );
+      break;
+
+    case "review":
+      returner = <ReviewView />;
+      break;
+    default:
+      returner = null;
+  }
+
+  return returner;
+}
+
+function ReviewView(props) {
+  function handleClick() {
+    if (props.pageIndex === "login") props.setPageIndex("review");
+  }
+
+  return (
+    <>
+      <h1>Gate 3: Peer Evaluation</h1>
+      <p>
+        Please fill in this form for each of the peers you worked with. The
+        evaluation will be shared with them anonymously. Try to provide feedback
+        that is constructive and that will help them grow as professionals.
+      </p>
+      <span>*Required</span>
+      <p>Feedback: Peer Evaluation</p>
+      <p>
+        Elabora tus ideas y ofrece ejemplos para que tu compañero tenga más
+        información y pueda tomar acción sobre tus observaciones.{" "}
+      </p>
+      <p>
+        ¿Cuál es el nombre de la primera persona a la que quieres brindar
+        feedback? *
+      </p>
+      <input type="text" />
+      <p>¿Cuales son sus principales fortalezas? *</p>
+      <input type="text" />
+      <p>¿Cómo contribuyó al equipo durante el Gate? *</p>
+      <input type="text" />
+      <p>
+        ¿Qué oportunidades de mejora tiene? Sé específico y trata de ofrecer
+        ejemplos *
+      </p>
+      <input type="text" />
+      <p>Please complete with honesty *</p>
+      <form onSubmit="">
+        <input type="radio" />
+        <input type="radio" />
+        <input type="radio" />
+        <input type="radio" />
+      </form>
+    </>
+  );
+}
+
+function LoginView(props) {
+  function handleClick() {
+    if (props.pageIndex === "login") props.setPageIndex("review");
+  }
+
+  return (
+    <>
+      <h1>Gate 3: Peer Evaluation</h1>
+      <p>
+        Please fill in this form for each of the peers you worked with. The
+        evaluation will be shared with them anonymously. Try to provide feedback
+        that is constructive and that will help them grow as professionals.
+      </p>
+      <span>*Required</span>
+      <span>¿Cual es tu nombre? *</span>
+      <input type="text" />
+      <span>This is a required question</span>
+      <button onClick={handleClick}>next</button>
+    </>
+  );
+}
+
+const $root = document.getElementById("root");
+render(<MainContainer />, $root);
